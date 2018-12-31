2af00c414882b33a0965e6abc8fa3c5ec50fcc55
Antoine THYS <contact@antoinethys.com>
2018-12-31 17:50:40 +0100

    Correction rebond

--- pong.py
+++ pong.py
@@ -12,7 +12,7 @@ def gameTick():
     global pos, vx, vy
     canvas.move(ball, vx, vy)
 
-    pos = canvas.coords(ball)
+    posb = canvas.coords(ball)
     posr1 = canvas.coords(r1)
     posr2 = canvas.coords(r2)
 
@@ -40,16 +40,16 @@ def gameTick():
         (posb[2] > posr1[0])
         and (posb[0] < posr1[2])
         and (posb[3] > posr1[1])
-        and posb[3] <= posr1[3]
+        and (posb[3] < posr1[3])
     ):
         vx = -v
         vy = random.randrange(-4, 4, 1)
     # Raquette gauche
     if (
-        (posb[2] > posr1[0])
-        and (posb[0] < posr1[2])
-        and (posb[3] > posr1[1])
-        and posb[3] <= posr1[3]
+        (posb[2] > posr2[0])
+        and (posb[0] < posr2[2])
+        and (posb[3] > posr2[1])
+        and (posb[3] < posr2[3])
     ):
         vx = v
         vy = random.randrange(-4, 4, 1)
@@ -104,13 +104,10 @@ ball = canvas.create_oval(x1, y1, x2, y2, fill="red", tag="ball")
 
 line = canvas.create_line(dla, 0, 540, hauteur, fill="white", dash=(20, 10), width=4)
 
+# Joueur Droit
 r1 = canvas.create_rectangle(1050, 270, 1070, 450, fill="red")
+# Joueur Gauche
 r2 = canvas.create_rectangle(10, 270, 30, 450, fill="red")
-
-# haut = canvas.create_line(1081, 1, 0, 1, fill="red", width=4)
-# bas = canvas.create_line(1081, 720, 0, 720, fill="red", width=4)
-# droite = canvas.create_line(0, 1, 1, 720, fill="red", width=4)
-# gauche = canvas.create_line(1081, 720, 1081, 1, fill="red", width=4)
 
 canvas.bind_all("<Up>", haut_d)
 canvas.bind_all("<Down>", bas_d)
